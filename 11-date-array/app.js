51d503335b88632aaed4ac59e05cd9ab99bff8ca
Evgenii Pushkarev <pushinbrazil@gmail.com>
2023-10-20 11:31:26 -0300

    Edits№2 to code-review-3

--- 11-date-array/app.js
+++ 11-date-array/app.js
@@ -37,7 +37,27 @@ function date (array) {
         ((el[2] === 4 || el[2] === 6 || el[2] === 9 || el[2] === 11) && el[0] <= 30) ||
         ((el[2] === 1 || el[2] === 3 || el[2] === 5 || el[2] === 7 || el[2] === 8 || el[2] === 10 || el[2] === 12) && el[0] <= 31))
     )
-    .map(el => el.join(''))    
+    .map(el => {
+    const joined = el.join('');
+    const split = joined.split('-');
+    const modified = split.map((subEl, i) => {
+        if ((i === 0 || i === 1) && subEl.length === 1) {
+            return '0' + subEl;
+        } 
+        return subEl;
+    });
+    return modified.join('-');
+});
+    // .map(el => el.join(''))
+    // .map(el => el.split('-'))
+    // .map(el => el.map((subEl, i) => {
+    //     if ((i === 0 || i === 1) && subEl.length === 1) {
+    //         return '0' + subEl
+    //     } 
+    //     return subEl
+    // }))
+    // .map(el => el.join('-'))
+
     return newArr
 }
 
